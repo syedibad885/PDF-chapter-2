0edbac540717cebbc909d63207b8ff748b3c4bf4
Syed ibad <155752000+syedibad885@users.noreply.github.com>
2024-03-18 16:33:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+//question no 1 //
+let username = "syed ibad"
+document.write(username);
+
+//question no 2 //
+let myname = "syed ibad"
+myname = "yasin"
+document.write(myname)
+
+//question no 3 //
+let massege;
+
+massege = "Hello Would";
+
+
+alert(massege);
+
+//question no 4 //
+
+let alertmassege = "Hello this is my webpage";
+
+alert(alertmassege);
+
+//question no 5 //
+let message = "welcome";
+ // Your message goes here
+alert(message);
+
+//question no 6 //
+
+let email = "syedoibad885@gmail.com";
+// Replace with your actual email address
+let messasge = "this is my email addersss: " + email;
+
+alert(massege);
+
+//question no 7 //
+
+let book = "this is may learning jave script ";
+
+alert(book);
+
+//question no 8 //
+let mymessage = "aslam alikoum"; 
+// Your message goes here
+alert(mymessage);
+
+//question no 9 //
+ let mystring = "syed ibad";
+
+ alert(mystring);
+
